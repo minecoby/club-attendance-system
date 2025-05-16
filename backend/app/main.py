413648c5703d5cd78c147b54dfc7e2e0aad32870
minecoby <minecoby@naver.com>
2025-05-16 17:24:18 +0900

    feat: 동시 api요청 시 처리할 수 있도록 구현

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,11 @@ from app.routes import admin
 from app.routes import club
 from app.routes import attend
 from fastapi.middleware.cors import CORSMiddleware
+
+import os
+from fastapi_limiter import FastAPILimiter
+from redis.asyncio import Redis
+
 app = FastAPI()
 
 app.add_middleware(
@@ -13,6 +18,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.on_event("startup")
+async def startup():
+    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+    redis = Redis.from_url(redis_url, encoding="utf-8", decode_responses=True)
+    await FastAPILimiter.init(redis)
 
 app.include_router(user.router,tags=["user"])
 app.include_router(admin.router,tags=["admin"])
--- backend/app/routes/attend.py
+++ backend/app/routes/attend.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,Security
+from fastapi import APIRouter, Depends, Security, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import get_db
@@ -8,29 +8,35 @@ from app.services.club_service import *
 from app.services.attend_service import *
 from app.routes.admin import attendance_ws 
 from app.schema.attend_schema import *
+import asyncio
+from fastapi_limiter.depends import RateLimiter
+
 security = HTTPBearer()
 
 router = APIRouter(
     prefix="/attend",
 )
 
-@router.post("/attendance/check")
+@router.post("/attendance/check", dependencies=[Depends(RateLimiter(times=100, seconds=10))])
 async def check_attendance(
-    data: AttendanceCheckRequest,credentials: HTTPAuthorizationCredentials = Security(security), db: AsyncSession = Depends(get_db)):
+    data: AttendanceCheckRequest, credentials: HTTPAuthorizationCredentials = Security(security), db: AsyncSession = Depends(get_db)):
     token = credentials.credentials
     user = await get_current_user(token, db)
-    club_code = await check_joining(user.user_id,data.club_code,db)
+    club_code = await check_joining(user.user_id, data.club_code, db)
     current = attendance_ws.attendance_codes.get(data.club_code)
-    
 
     if not current:
         raise HTTPException(status_code=404, detail="출석 코드가 활성화되어 있지 않습니다.")
 
     if current["code"] != data.code:
         raise HTTPException(status_code=400, detail="출석 코드가 일치하지 않습니다.")
+    
     date = current["date"]
     date_obj = datetime.strptime(date, "%Y-%m-%d").date()
-    date_id = await get_date_id(date_obj,data.club_code,db)
-    await attend_date(user.user_id,date_id,db)
+    date_id = await get_date_id(date_obj, data.club_code, db)
+    
+    await asyncio.gather(
+        attend_date(user.user_id, date_id, db)
+    )
 
     return {"message": "출석이 확인되었습니다."}
